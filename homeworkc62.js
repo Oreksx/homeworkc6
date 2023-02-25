bd278222a6fd143f2ea2505a9bedaff9dbafe955
Oreksx <104359192+Oreksx@users.noreply.github.com>
2023-02-25 15:52:21 +0300

    Create homeworkc62.js

---
+++ homeworkc62.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.j-btn-test');
+
+btn.addEventListener('click', () => {
+  alert(`размер экрана с учетом полосы прокрутки: ${window.innerWidth} x ${window.innerHeight}`);
+  });
+
+
+
+
+
+
